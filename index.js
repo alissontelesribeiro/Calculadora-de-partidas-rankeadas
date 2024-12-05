82e9735df5b779bac1e5fe2ba240abf967fbd5a6
alissonteles <alisson_teles_ribeio@outlook.com>
2024-12-05 15:25:39 -0300

    acabei

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let resultado = rank(7,2)
-
-console.log(resultado)
+let resultado = rank(150,2)
 
 function rank(vitorias, derrotas){
     let rankeadas = vitorias - derrotas
-    return rankeadas
+    if (vitorias < 10){
+        console.log("O herói tem saldo de " + rankeadas + " e está no nível de Ferro")
+    }else if (vitorias >= 11 && vitorias <= 20){
+        console.log("O herói tem saldo de " + rankeadas + " e está no nível de Bronze")
+    }else if (vitorias >= 21 && vitorias <= 50){
+        console.log("O herói tem saldo de " + rankeadas + " e está no nível de Prata")
+    }else if (vitorias >= 51 && vitorias <= 80){
+        console.log("O herói tem saldo de " + rankeadas + " e está no nível de Ouro")
+    }else if (vitorias >= 81 && vitorias <= 90){
+        console.log("O herói tem saldo de " + rankeadas + " e está no nível de Diamante")
+    }else if (vitorias >= 91 && vitorias <= 100){
+        console.log("O herói tem saldo de " + rankeadas + " e está no nível de Lendário")
+    }else if (vitorias >= 101){
+        console.log("O herói tem saldo de " + rankeadas + " e está no nível de Imortal")
+    }
 }
